e47133dc02ddde268c9b8f8a567e709019d38fce
Osamazzz <1176091035@qq.com>
2022-03-16 21:48:38 +0800

    蓝桥杯练习

--- src/lanqiaobei/Exercise_/Exercise02.java
+++ src/lanqiaobei/Exercise_/Exercise02.java
@@ -10,27 +10,13 @@ public class Exercise02 {
     public static void main(String[] args) {
         //使用一个数组A来表示一个大整数a，A[0]表示a的个位，A[1]表示a的十位，依次类推
         int[] arr = new int[1000];
-        int num = 10;
-        int count = 0;
+        int num = 1;
         Factorial(num, arr);
-        for (int i = arr.length - 1; i >= 0; i--) {//从高位开始扫描
-            if (arr[i] != 0)
-                break;
-            count++;
-        }
-        int length = arr.length - count;
-        int[] arr2 = new int[length];
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = arr[i];
-        }
-        for (int i = arr2.length - 1; i >= 0; i--) {
-            System.out.print(arr2[i]);
-        }
     }
-
     public static void Factorial(int n, int[] arr) {
         int i = 1;//乘数
         arr[0] = 1;
+        int count = 0;
         if (n == 0)//0!=1
             return;//退出
         while (i <= n) {
@@ -45,6 +31,19 @@ public class Exercise02 {
                 }
             }
             i++;
+        }
+        for (int j = arr.length - 1; j >= 0; j--) {//从高位开始扫描
+            if (arr[j] != 0)
+                break;
+            count++;
+        }
+        int length = arr.length - count;
+        int[] arr2 = new int[length];
+        for (int j = 0; j < arr2.length; j++) {
+            arr2[j] = arr[j];
+        }
+        for (int j = arr2.length - 1; j >= 0; j--) {
+            System.out.print(arr2[j]);
         }
     }
 }
